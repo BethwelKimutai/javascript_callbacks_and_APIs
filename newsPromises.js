182d033129215b7dc7f16082f11b38d1b51d127e
Bethwel Kimutai <131645563+BethwelKimutai@users.noreply.github.com>
2023-10-16 15:04:34 +0300

    Add files via upload

---
+++ newsPromises.js
@@ -0,0 +1,51 @@
+const newsApiUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=44eca781d6fb4e2da8fd8e7969b3b375';
+const newsContainer = document.getElementById('news');
+
+// Fetch news data using Promises
+function getNewsData(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch news data');
+      }
+      return response.json();
+    });
+}
+
+// Display news content using Promises
+function displayNews(data) {
+  if (data.articles && data.articles.length > 0) {
+    const articles = data.articles;
+
+    // Create HTML markup for each article
+    const newsHTML = articles.map(article => `
+      <article class="news-article">
+        <h2>${article.title}</h2>
+        <p>${article.description}</p>
+        <a href="${article.url}" target="_blank">Read more</a>
+      </article>
+    `).join('');
+
+    // Update the news container with the generated HTML
+    newsContainer.innerHTML = newsHTML;
+  } else {
+    newsContainer.innerHTML = '<p>No news available at the moment.</p>';
+  }
+}
+
+// Add an event listener to a button for fetching news
+document.querySelector('button').addEventListener('click', () => {
+  // Change the button text to indicate loading
+  document.querySelector('button').textContent = 'Loading...';
+
+  // Fetch news data using Promises
+  getNewsData(newsApiUrl)
+    .then(displayNews)
+    .catch(error => {
+      console.error(error);
+      newsContainer.innerHTML = '<p>Failed to fetch news data.</p>';
+    })
+    .finally(() => {
+      document.querySelector('button').textContent = 'View all the news';
+    });
+});
